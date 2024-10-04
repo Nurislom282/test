f52d9bfc753aea4010fee65735369d6f83ce60ca
Nurislom <anurislom425@gmail.com>
2024-10-04 19:13:50 +0500

    person class main.py ga qoshdim

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def summa(a,b):
     return a+b
 
 print(summa(4,5))
+
+class Person():
+    def __init__(self,name,last_name):
+        self.name = name
+        self.last_name = last_name
+        
